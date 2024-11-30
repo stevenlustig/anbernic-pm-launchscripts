e6909534281093f879c8b8086858075f276ce6e6
Manuel Rodríguez Matesanz <manurocker95@gmail.com>
2024-11-30 11:40:50 +0100

    Update Pokemon Reborn.sh

--- Pokemon Reborn.sh
+++ Pokemon Reborn.sh
@@ -28,11 +28,21 @@ GAMEDIR="$PORTDIR/reborn"
 
 mkdir -p "$GAMEDIR/conf"
 
-# Enable logging
-> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+## TODO: Change to PortMaster/tty when Johnnyonflame merges the changes in,
+$ESUDO chmod 666 /dev/tty0
+printf "\033c" > /dev/tty0
+echo "Loading... Please Wait." > /dev/tty0
+
+CUR_TTY=/dev/tty0
+
+$ESUDO chmod 666 $CUR_TTY
+$ESUDO touch log.txt
+$ESUDO chmod 666 log.txt
+export TERM=linux
+printf "\033c" > $CUR_TTY
 
 # Exports
-export LD_LIBRARY_PATH="$GAMEDIR/libs.${DEVICE_ARCH}:/usr/lib:/usr/lib32:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$GAMEDIR/libs.aarch64:/usr/lib:/usr/lib32:$LD_LIBRARY_PATH"
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
 export TEXTINPUTINTERACTIVE="Y"
 export XDG_DATA_HOME="$CONFDIR"
@@ -40,13 +50,25 @@ export LC_ALL=C
 export LANG=C
 cd $GAMEDIR
 
+
+
 # Move the mkxp.json preset
 mv preset/mkxp.json ./mkxp.json
 
 # Gptk and run port
-$GPTOKEYB "mkxp-z.${DEVICE_ARCH}" -c "./reborn.gptk" &
-pm_platform_helper $GAMEDIR/mkxp-z.${DEVICE_ARCH}
-./mkxp-z.${DEVICE_ARCH}
+$GPTOKEYB "mkxp-z.aarch64" -c "./reborn.gptk" &
+SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+
+pm_platform_helper $GAMEDIR/mkxp-z.aarch64
+./mkxp-z.aarch64
 
 # Cleanup
 pm_finish
+
+$ESUDO kill -9 $(pidof gptokeyb)
+unset LD_LIBRARY_PATH
+unset SDL_GAMECONTROLLERCONFIG
+$ESUDO systemctl restart oga_events &
+
+# Disable console
+printf "\033c" > $CUR_TTY
